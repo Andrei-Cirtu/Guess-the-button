ca2c50c37fbf23f916372c148e04c8adc5c620ba
Andrei-Cirtu <92400247+Andrei-Cirtu@users.noreply.github.com>
2021-11-17 14:23:20 +0100

    Update guess_the_button.js

--- guess_the_button.js
+++ guess_the_button.js
@@ -3,14 +3,23 @@ function myFunction() {
     if (isNaN(times)) {
         alert("Invalid input");
     } else {
-        for (var i = 0; i < times; ++i) {
+        var winner_button = Math.floor(Math.random() * times) + 1;
+        //alert ("winner is " + winner_button);
+        for (var i = 1; i <= times; ++i) {
             buton.style.display = 'none';
             let btn = document.createElement("button");
             btn.innerHTML = "Save";
             document.body.appendChild(btn);
-            btn.onclick = function winner () {
+            if (i == winner_button) {
+                btn.onclick = function winner () {
+                alert ("Congrats! You won!");
+                document.location.reload();
+                }
+            } else {
+                btn.onclick = function loser () {
                 alert ("This is not the winner");
                 document.location.reload();
+                }
             }
             console.log('clicked')
         }
